db5fe499e293b0938403b7844fde7161b69069a6
Nisida Azzalini <nisida.azzalini@gmail.com>
2020-06-15 11:31:38 +1200

    create About.jsx and import it to App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
 import React from "react";
-import { HashRouter as Router } from "react-router-dom";
-import { Switch, Route } from "react-router";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import SideNav from "./components/SideNav";
 import Home from "./components/Home";
+import About from "./components/About";
 
 class App extends React.Component {
   render() {
     return (
       <Router>
-        <SideNav />
+        <Route component={SideNav} />
         <div className="page-container">
           <Switch>
-            <Route exact path="/">
-              <Home />
-            </Route>
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
           </Switch>
         </div>
       </Router>
