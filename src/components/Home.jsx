4073988cd3bfe025b64be6d81b75c60d4fd1489c
Nisida Azzalini <nisida.azzalini@gmail.com>
2020-06-14 19:04:36 +1200

    break intro text in 3 lines

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import "./Home.css"
+import nl2br from 'react-newline-to-break'; 
 
 class Home extends React.Component {
     render(){
+        let introText = "Hi, \nI'm Nisida,\nWeb Developer."
         return (
             <div className="home-container">
-                <p>Hello Home!</p>
+                <div className="intro-text">
+                    {nl2br(introText)}
+                </div>
             </div>
         )
     }
