fe066f49f3cdb4f1a3421c88026bd508b4f51fd5
Nisida Azzalini <nisida.azzalini@gmail.com>
2020-06-14 18:06:15 +1200

    Add Home.jsx in App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React from "react";
+import { HashRouter as Router } from "react-router-dom";
+import { Switch, Route } from "react-router";
 import "./App.css";
 import SideNav from "./components/SideNav";
+import Home from "./components/Home";
 
 class App extends React.Component {
   render() {
     return (
-      <div className="page-container">
+      <Router>
         <SideNav />
-      </div>
+        <div className="page-container">
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
