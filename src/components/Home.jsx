ae3c20dd9fc24f2beb004e8435af76e5bebd7763
Nisida Azzalini <nisida.azzalini@gmail.com>
2020-06-17 11:18:57 +1200

    style intro Home page with some animation

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,19 @@
 import React from "react";
 import { Col } from "reactstrap";
 import "./Style/Home.css";
-import nl2br from "react-newline-to-break";
+// import nl2br from "react-newline-to-break";
 
 class Home extends React.Component {
   render() {
-    let introText = "Hi, \nI'm Nisida,\nWeb Developer.";
+    // let introText = "Hi, \nI'm Nisida,\nWeb Developer.";
     return (
       <div className="home-container">
         <Col>
-          <div>{nl2br(introText)}</div>
+          <div className="animation-typewriter">
+            <p>Hi,</p>
+            <p>I'm Nisida,</p>
+            <p>Web Developer.</p>
+          </div>
         </Col>
       </div>
     );
