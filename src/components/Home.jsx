c1551f0456716ede80c413709f79843454a82901
Nisida Azzalini <nisida.azzalini@gmail.com>
2020-06-17 17:39:19 +1200

    make each letter in intro change color when hovering

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,14 +10,44 @@ class Home extends React.Component {
       <Row className="home-container">
         <Col sm="6">
           <div className="animation-typewriter">
-            <p>Hi,</p>
-            <p>I'm Nisida,</p>
-            <p>Web Developer.</p>
+            <p>
+              <span className="color-change first-letter">H</span>
+              <span className="color-change">i</span>
+              <span className="color-change">,</span>
+            </p>
+            <p>
+              <span className="color-change">I</span>
+              <span className="color-change">'</span>
+              <span className="color-change">m</span>
+              <span className="color-change">&nbsp;</span>
+              <span className="color-change name-letter">N</span>
+              <span className="color-change name-letter">i</span>
+              <span className="color-change name-letter">s</span>
+              <span className="color-change name-letter">i</span>
+              <span className="color-change name-letter">d</span>
+              <span className="color-change name-letter">a</span>
+              <span className="color-change">,</span>
+            </p>
+            <p>
+              <span className="color-change">W</span>
+              <span className="color-change">e</span>
+              <span className="color-change">b</span>
+              <span className="color-change">&nbsp;</span>
+              <span className="color-change">D</span>
+              <span className="color-change">e</span>
+              <span className="color-change">v</span>
+              <span className="color-change">e</span>
+              <span className="color-change">l</span>
+              <span className="color-change">o</span>
+              <span className="color-change">p</span>
+              <span className="color-change">e</span>
+              <span className="color-change">r</span>
+              <span className="color-change">.</span>
+            </p>
           </div>
         </Col>
         <Col sm="6">
-          <div>
-          </div>
+          <div></div>
         </Col>
       </Row>
     );
