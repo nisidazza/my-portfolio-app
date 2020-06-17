5f58e6f906b7be819ac6ab1f0dd5d7d12f64acf4
Nisida Azzalini <nisida.azzalini@gmail.com>
2020-06-17 12:18:03 +1200

    add columns to Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Col } from "reactstrap";
+import { Col, Row } from "reactstrap";
 import "./Style/Home.css";
 // import nl2br from "react-newline-to-break";
 
@@ -7,15 +7,19 @@ class Home extends React.Component {
   render() {
     // let introText = "Hi, \nI'm Nisida,\nWeb Developer.";
     return (
-      <div className="home-container">
-        <Col>
+      <Row className="home-container">
+        <Col sm="6">
           <div className="animation-typewriter">
             <p>Hi,</p>
             <p>I'm Nisida,</p>
             <p>Web Developer.</p>
           </div>
         </Col>
-      </div>
+        <Col sm="6">
+          <div>
+          </div>
+        </Col>
+      </Row>
     );
   }
 }
