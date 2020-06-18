d06053f24993dfbc7c216277932c03c6f6b3cce7
Nisida Azzalini <nisida.azzalini@gmail.com>
2020-06-18 15:22:05 +1200

    fix height and width of the skills container

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,39 +1,43 @@
 import React from "react";
-import { Col } from "reactstrap";
+import { Col, Row } from "reactstrap";
 import "./Style/Skills.css";
 import "./Style/CommonCSS.css";
 
 class Skills extends React.Component {
   render() {
     return (
-      <div className="skills-container">
-        <Col>
-          <h1 className="title-animation">
-            <span>S</span>
-            <span>k</span>
-            <span>i</span>
-            <span>l</span>
-            <span>l</span>
-            <span>s</span>
+      <Row className="skills-container">
+        <Col sm="6">
+          <div className="skills-list-container">
+            <h1 className="title-animation">
+              <span>S</span>
+              <span>k</span>
+              <span>i</span>
+              <span>l</span>
+              <span>l</span>
+              <span>s</span>
             </h1>
-          <h5>Front End Development</h5>
-          <ul>
-            <li>HTML</li>
-            <li>CSS</li>
-            <li>JavaScript</li>
-            <li>React</li>
-            <li>Bootstrap</li>
-          </ul>
-          <h5>Back End Development</h5>
-          <ul>
-            <li>Node.js</li>
-            <li>Express.js</li>
-            <li>SQLite3 / PostgreSQL</li>
-            <li>Knex.js</li>
-            <li>REST API</li>
-          </ul>
+            <h5>Front End Development</h5>
+            <ul>
+              <li>HTML</li>
+              <li>CSS</li>
+              <li>JavaScript</li>
+              <li>React</li>
+              <li>Bootstrap</li>
+            </ul>
+            <h5>Back End Development</h5>
+            <ul>
+              <li>Node.js</li>
+              <li>Express.js</li>
+              <li>SQLite3 / PostgreSQL</li>
+              <li>Knex.js</li>
+              <li>REST API</li>
+            </ul>
+          </div>
         </Col>
-      </div>
+        <Col sm="6">
+        </Col>
+      </Row>
     );
   }
 }
